5a83a71dd37e52f30900d64315b238666785dd39
illupovero <davideluporini1996@gmail.com>
2021-12-21 16:21:58 +0100

    added ex track and linked js

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// TRACCIA
+
+/*
+Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere 
+e l'età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, 
+secondo queste regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.
+L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, 
+    per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+*/
+
+// 1) CHIDERE IL NUMERO DI KM CHE SI DESIDERA PERCORRERE
+// 2)CHIEDERE L'ETA' DEL PASSEGGERO
+
+console.log('js ok')
+
+const distancePrefered = prompt('Quale distanza desideri percorrere? (in km)')
+console.log(distancePrefered)
